79d100e0839377d966afe9158915ea896869627b
Leonardo Silva Kuligovski <leonardokuligoski@gmail.com>
2023-04-25 19:35:38 -0300

    Global Style and Header finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import "./global.scss";
+
+import styles from "./App.module.scss";
+
+import rocketIcon from "./assets/rocket-icon.svg";
+
 export function App() {
-  return <>Start Project</>;
+  return (
+    <div className={styles.app}>
+      <header>
+        <img src={rocketIcon} alt="Rocket Icon" />
+        <div>
+          <span>to</span>
+          <span>do</span>
+        </div>
+      </header>
+      <section>Another</section>
+    </div>
+  );
 }
 
 export default App;
